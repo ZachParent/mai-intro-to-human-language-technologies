f355fcfa276c934282e13215359c3c8fb154f0ad
Zach Parent <zachparent@duck.com>
2024-11-15 13:07:30 +0100

    add config.py

---
+++ project/src/zp_ihlt_project/config.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).parent.parent.parent
+DATA_DIR = PROJECT_ROOT / "data"
+PROVIDED_DATA_DIR = DATA_DIR / "provided"
+TRAIN_DATA_DIR = PROVIDED_DATA_DIR / "train"
+TEST_DATA_DIR = PROVIDED_DATA_DIR / "test-gold"
