45a90e07a3345590b3278fa74ffd89a4fa480061
Zach Parent <zachparent@duck.com>
2024-11-15 17:16:44 +0100

    make format

--- project/src/zp_ihlt_project/feature_generation.py
+++ project/src/zp_ihlt_project/feature_generation.py
@@ -4,47 +4,64 @@ from typing import List, Callable, Dict, Tuple
 
 nlp = spacy.load("en_core_web_sm")
 
+
 class PosTag(str):
     """A special string type for part-of-speech tags"""
+
     pass
+
 
 class Word(str):
     """A special string type for words"""
+
     pass
+
 
 # Spacy methods
 def sentence_to_doc(sentence: str) -> spacy.tokens.doc.Doc:
     return nlp(sentence)
 
+
 def get_tokens(doc: spacy.tokens.doc.Doc) -> List[spacy.tokens.token.Token]:
     return [token for token in doc]
+
 
 def get_pos_tags(doc_or_tokens: List[spacy.tokens.token.Token]) -> List[PosTag]:
     return [token.pos_ for token in doc_or_tokens]
 
+
 def lemmatize_tokens(tokens: List[spacy.tokens.token.Token]) -> List[Word]:
     return [token.lemma_ for token in tokens]
 
+
 def get_token_text(tokens: List[spacy.tokens.token.Token]) -> List[Word]:
     return [token.text for token in tokens]
+
 
 def chunk_NEs(doc: spacy.tokens.doc.Doc) -> List[spacy.tokens.token.Token]:
     with doc.retokenize() as retokenizer:
         tokens = [token for token in doc]
         for ent in doc.ents:
-            retokenizer.merge(doc[ent.start:ent.end], 
-                attrs={"LEMMA": " ".join([tokens[i].text 
-                                    for i in range(ent.start, ent.end)])})
+            retokenizer.merge(
+                doc[ent.start : ent.end],
+                attrs={"LEMMA": " ".join([tokens[i].text for i in range(ent.start, ent.end)])},
+            )
     return [token for token in doc]
 
-def remove_non_alnum(words : List[Word]) -> List[Word]:
+
+def remove_non_alnum(words: List[Word]) -> List[Word]:
     return [word for word in words if word.isalnum()]
 
-def lower(words : List[Word]) -> List[Word]:
+
+def lower(words: List[Word]) -> List[Word]:
     return [word.lower() for word in words]
 
-def remove_stopwords(doc_or_tokens: List[spacy.tokens.token.Token]) -> List[spacy.tokens.token.Token]:
+
+def remove_stopwords(
+    doc_or_tokens: List[spacy.tokens.token.Token],
+) -> List[spacy.tokens.token.Token]:
     return [token for token in doc_or_tokens if not token.is_stop]
+
 
 # List of processing functions
 functions = [
@@ -55,7 +72,7 @@ functions = [
     get_token_text,
     remove_non_alnum,
     lower,
-    remove_stopwords
+    remove_stopwords,
 ]
 
 # List of processing functions with their input and output types
@@ -70,6 +87,7 @@ function_input_output_types: Dict[str, Tuple[Callable, Tuple[type, type]]] = {
     remove_stopwords.__name__: (List[spacy.tokens.token.Token], List[spacy.tokens.token.Token]),
 }
 
+
 # Function to check if a permutation is valid based on input/output types
 def is_valid_permutation(perm: List[str]) -> bool:
     if function_input_output_types[perm[0].__name__][0] != str:
@@ -83,6 +101,7 @@ def is_valid_permutation(perm: List[str]) -> bool:
         if current_func_output_type != next_func_input_type:
             return False
     return True
+
 
 def generate_valid_permutations() -> List[List[Callable]]:
     valid_permutations = []
