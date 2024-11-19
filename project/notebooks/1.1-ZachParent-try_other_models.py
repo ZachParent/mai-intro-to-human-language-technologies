adbc1301397790bbf7b3fbf1c779634102bccfa2
Zach Parent <zachparent@duck.com>
2024-11-19 15:08:03 +0100

    use gridsearch

--- project/notebooks/1.1-ZachParent-try_other_models.py
+++ project/notebooks/1.1-ZachParent-try_other_models.py
@@ -39,18 +39,47 @@ train_df.head()
 
 # %%
 from sklearn.neural_network import MLPRegressor
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 
 X_train = train_df[features]
 y_train = train_df.gs
 X_test = test_df[features]
 y_test = test_df.gs
 
-mlp = MLPRegressor(hidden_layer_sizes=(200, 20), 
-                  activation='relu',
-                  solver='adam')
-mlp.fit(X_train, y_train)
-preds = mlp.predict(X_test)
+# Define parameter grid
+param_grid = {
+    'hidden_layer_sizes': [(200,40), (200, 20), (400, 40), (200, 80), (400, 80), (400, 20)],
+    'activation': ['tanh'],
+    'solver': ['adam']
+}
+
+# Create MLPRegressor
+mlp = MLPRegressor(max_iter=500, random_state=42)
+
+# Perform grid search with 5-fold cross validation
+grid_search = GridSearchCV(
+    mlp, 
+    param_grid,
+    cv=5,
+    scoring='neg_mean_squared_error',
+    n_jobs=-1,
+    verbose=1
+)
+
+# Fit grid search
+grid_search.fit(X_train, y_train)
+
+# Print best parameters and score
+print("Best parameters:", grid_search.best_params_)
+print("Best RMSE:", np.sqrt(-grid_search.best_score_))
+
+best_mlp = grid_search.best_estimator_
+preds = best_mlp.predict(X_train)
+pearsonr(y_train, preds)
+
+# %%
+best_mlp = grid_search.best_estimator_
+preds = best_mlp.predict(X_test)
 pearsonr(y_test, preds)
 
 # %% [markdown]
