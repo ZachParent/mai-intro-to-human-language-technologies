fe8f22d39851e0240e119bc5412ae850f4a6c5dd
Zach Parent <zachparent@duck.com>
2024-11-21 15:31:10 +0100

    start extract_features and train_model scripts

---
+++ project/notebooks/train_model.py
@@ -0,0 +1,191 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     custom_cell_magics: kql
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: .venv-ihlt
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import spacy
+from typing import Tuple
+import nltk
+import numpy as np
+import pandas as pd
+from functools import partial
+from sklearn.neural_network import MLPRegressor
+from sklearn.svm import SVR
+from sklearn.model_selection import GridSearchCV
+from scipy.stats import pearsonr
+
+from zp_ihlt_project.config import TRAIN_DATA_DIR, TEST_DATA_DIR
+
+
+# %%
+all_train_dt = pd.read_csv("../data/processed/train_data_with_features.csv")
+all_test_dt = pd.read_csv("../data/processed/test_data_with_features.csv")
+feature_names = [col for col in all_train_dt.columns if col.startswith("score_")]
+
+# %%
+from sklearn.feature_selection import SelectKBest, mutual_info_regression
+
+kbest = SelectKBest(mutual_info_regression, k=600).fit(all_train_dt[feature_names], all_train_dt.gs)
+selected_features = np.array(feature_names)[kbest.get_support()]
+
+# %%
+train_datasets = all_train_dt.dataset.unique().tolist()
+test_datasets = all_test_dt.dataset.unique().tolist()
+
+# %%
+results = []
+
+for dataset in train_datasets:
+    dataset_results = []
+    dataset_results.append(dataset)
+    dt = all_train_dt[all_train_dt.dataset == dataset]
+    test_dt = all_test_dt[all_test_dt.dataset == dataset]
+
+    X_train = dt[selected_features]
+    y_train = dt.gs
+    X_test = test_dt[selected_features]
+    y_test = test_dt.gs
+
+    # Define parameter grid
+    param_grid = {
+        'C': [0.01, 0.1, 1, 10],
+        'kernel': ['linear', 'rbf', 'poly'],
+        'gamma': ['scale', 'auto'],
+        'epsilon': [0.1]
+    }
+
+    svr = SVR()
+
+    # Perform grid search with 5-fold cross validation
+    grid_search = GridSearchCV(
+        svr, 
+        param_grid,
+        cv=5,
+        scoring='neg_mean_squared_error',
+        n_jobs=-1,
+        verbose=1
+    )
+
+    # Fit grid search
+    grid_search.fit(X_train, y_train)
+
+    # Print best parameters and score
+    print("Best parameters:", grid_search.best_params_)
+    print("Best RMSE:", np.sqrt(-grid_search.best_score_))
+
+    best_mlp = grid_search.best_estimator_
+    preds = best_mlp.predict(X_train)
+    dataset_results.append(pearsonr(y_train, preds)[0])
+
+    best_mlp = grid_search.best_estimator_
+    preds = best_mlp.predict(X_test[selected_features])
+    dataset_results.append(pearsonr(y_test, preds)[0])
+
+    results.append(dataset_results)
+
+results = pd.DataFrame(results, columns=["dataset", "train_pearson", "test_pearson"])
+results
+# %%
+
+# %%
+dt = all_train_dt
+
+X_train = dt[selected_features]
+y_train = dt.gs
+
+# Define parameter grid
+param_grid = {
+    'C': [0.01, 0.1, 1, 10],
+    'kernel': ['linear', 'rbf', 'poly'],
+    'gamma': ['scale', 'auto'],
+    'epsilon': [0.1]
+}
+
+svr = SVR()
+
+# Perform grid search with 5-fold cross validation
+grid_search = GridSearchCV(
+    svr, 
+    param_grid,
+    cv=5,
+    scoring='neg_mean_squared_error',
+    n_jobs=-1,
+    verbose=1
+)
+
+# Fit grid search
+grid_search.fit(X_train, y_train)
+
+# Print best parameters and score
+print("Best parameters:", grid_search.best_params_)
+print("Best RMSE:", np.sqrt(-grid_search.best_score_))
+
+best_mlp = grid_search.best_estimator_
+preds = best_mlp.predict(X_train)
+pearsonr(y_train, preds)
+
+# %%
+train_results = []
+datasets = train_datasets
+for dataset in datasets:
+    dataset_results = []
+    dataset_results.append(dataset)
+    dt = all_train_dt[all_train_dt.dataset == dataset]
+    test_dt = all_test_dt[all_test_dt.dataset == dataset]
+
+    X_train = dt[selected_features]
+    y_train = dt.gs
+    X_test = test_dt[selected_features]
+    y_test = test_dt.gs
+
+    preds = best_mlp.predict(X_train)
+    dataset_results.append(pearsonr(y_train, preds)[0])
+
+    preds = best_mlp.predict(X_test[selected_features])
+    dataset_results.append(pearsonr(y_test, preds)[0])
+
+    train_results.append(dataset_results)
+
+train_results = pd.DataFrame(train_results, columns=["dataset", "train_pearson", "test_pearson"])
+train_results
+# %%
+
+
+# %%
+test_results = []
+datasets = test_datasets
+for dataset in datasets:
+    dataset_results = []
+    dataset_results.append(dataset)
+    test_dt = all_test_dt[all_test_dt.dataset == dataset]
+
+    X_test = test_dt[selected_features]
+    y_test = test_dt.gs
+
+    preds = best_mlp.predict(X_test[selected_features])
+    dataset_results.append(pearsonr(y_test, preds)[0])
+
+    test_results.append(dataset_results)
+
+test_results = pd.DataFrame(test_results, columns=["dataset", "test_pearson"])
+test_results
+# %%
+
+
+# %%
+best_mlp = grid_search.best_estimator_
+preds = best_mlp.predict(X_test[selected_features])
+pearsonr(y_test, preds)
+
