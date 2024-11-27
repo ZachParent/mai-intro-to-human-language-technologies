09983b0635d601ab54aeaa34ed19b95b5f48a49f
Zach Parent <zachparent@duck.com>
2024-11-27 17:12:02 +0100

    include feature names in feature_steps tables

--- project/src/zp_ihlt_project/config.py
+++ project/src/zp_ihlt_project/config.py
@@ -6,5 +6,12 @@ PROVIDED_DATA_DIR = DATA_DIR / "provided"
 TRAIN_DATA_DIR = PROVIDED_DATA_DIR / "train"
 TEST_DATA_DIR = PROVIDED_DATA_DIR / "test-gold"
 
+PROCESSED_DATA_DIR = DATA_DIR / "processed"
+TRAIN_DATA_WITH_FEATURES_PATH = PROCESSED_DATA_DIR / "train_data_with_features.csv"
+TEST_DATA_WITH_FEATURES_PATH = PROCESSED_DATA_DIR / "test_data_with_features.csv"
+
+FEATURE_STEPS_PATH = DATA_DIR / "feature_steps.csv"
+FEATURE_STEPS_WITH_IMPORTANCE_PATH = DATA_DIR / "feature_steps_with_importance.csv"
+
 INPUT_FILENAME_PATTERN = "STS.input.{}.txt"
 GS_FILENAME_PATTERN = "STS.gs.{}.txt"
